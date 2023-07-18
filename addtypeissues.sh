c18bfc96f7fad85cbaaff6eeb17d8b8f4816075d
glhl86 <glhl86@gmail.com>
2023-07-18 01:31:51 -0500

    issue error

--- addtypeissues.sh
+++ addtypeissues.sh
@@ -1,5 +1,8 @@
 mkdir -p .github/ISSUE_TEMPLATE/
 wget -O .github/ISSUE_TEMPLATE/1-reportar-error.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/1-reportar-error.md
+wget -O .github/ISSUE_TEMPLATE/2-solicitar-nueva-funcionalidad-o-mejora.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/2-solicitar-nueva-funcionalidad-o-mejora.md
+wget -O .github/ISSUE_TEMPLATE/3-documentacion.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/3-documentacion.md
+wget -O .github/ISSUE_TEMPLATE/config.yml https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/config.yml
 git config user.name bot-bancolombia-toolkit
 git config user.email oficina_open_source@bancolombia.com.co
 git add .
