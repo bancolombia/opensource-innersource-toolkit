62057855b21ea4b48c9be70b51952752d259ec0d
glhl86 <glhl86@gmail.com>
2023-07-18 01:19:52 -0500

    issue error

--- addtypeissues.sh
+++ addtypeissues.sh
@@ -1,1 +1,2 @@
 mkdir -p .github/ISSUE_TEMPLATE/
+wget -O .github/ISSUE_TEMPLATE/1-reportar-error.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/1-reportar-error.md
