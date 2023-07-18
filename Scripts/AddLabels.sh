d8d3be552c22e2ac1e43ab5308bfc65ae02d6844
Guillermo leon hoyos lopez <glhl86@gmail.com>
2023-07-18 00:04:02 -0500

    Update AddLabels.sh

--- Scripts/AddLabels.sh
+++ Scripts/AddLabels.sh
@@ -1,1 +1,10 @@
-
+url_file_labels="https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Configurations/labels.txt"
+curl -s "$url_file_labels" | while IFS= read -r line;
+do
+   IFS_antiguo=$IFS
+   IFS=";" 
+   read -ra partes <<< "$line"
+   IFS=$IFS_antiguo
+   gh label create "${partes[0]}" --description "${partes[1]}" --color "${partes[2]}" -f
+   echo "${partes[0]}"
+done
