746dd484231b770dbfa78c49e032e3a2e955e467
glhl86 <glhl86@gmail.com>
2023-07-18 01:23:37 -0500

    issue error

--- addtypeissues.sh
+++ addtypeissues.sh
@@ -1,2 +1,5 @@
 mkdir -p .github/ISSUE_TEMPLATE/
 wget -O .github/ISSUE_TEMPLATE/1-reportar-error.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/1-reportar-error.md
+git add .
+git commit -m "Changues bot action toolkit innersource"
+git push
