5ae79577e225f331c80c1f50d99785fc910cfce5
glhl86 <glhl86@gmail.com>
2023-07-19 12:30:20 -0500

    uplodad templates documentation

--- addworkflowstemplates.sh
+++ addworkflowstemplates.sh
@@ -3,8 +3,3 @@ wget -O .github/ISSUE_TEMPLATE/1-reportar-error.md https://raw.githubusercontent
 wget -O .github/ISSUE_TEMPLATE/2-solicitar-nueva-funcionalidad-o-mejora.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/2-solicitar-nueva-funcionalidad-o-mejora.md
 wget -O .github/ISSUE_TEMPLATE/3-documentacion.md https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/3-documentacion.md
 wget -O .github/ISSUE_TEMPLATE/config.yml https://raw.githubusercontent.com/bancolombia/action-innersource-toolkit/main/Templates/IssueTemplates/config.yml
-git config user.name bot-bancolombia-toolkit
-git config user.email oficina_open_source@bancolombia.com.co
-git add .
-git commit -m "Changues bot action toolkit innersource"
-git push
