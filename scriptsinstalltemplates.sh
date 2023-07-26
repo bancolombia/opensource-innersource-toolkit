b0a6c0533c793429744595ee8994c9309f651f01
glhl86 <glhl86@gmail.com>
2023-07-26 14:56:21 -0500

    add support to english and spanish

--- scriptsinstalltemplates.sh
+++ scriptsinstalltemplates.sh
@@ -27,7 +27,7 @@ gh repo edit --enable-discussions --enable-wiki --enable-projects=true
 #Create project to reviews
 REPO_NAME=$(gh repo view $VAR_NAME_REPOSITORY --json name --jq '.name')
 
-PROJECT_NAME=$(gh project list --json name --jq '.name')
+PROJECT_NAME=$(gh project list)
 
 echo $PROJECT_NAME
 #gh project create --owner $VAR_NAME_REPOSITORY_OWNER --title "Backlog/Reviews-$REPO_NAME" 
