daab350345ad0a61b4da8fe624f8dc3af44068cc
glhl86 <glhl86@gmail.com>
2023-07-27 08:56:12 -0500

    add support to english and spanish

--- uploadchangues.sh
+++
@@ -1,5 +0,0 @@
-git config user.name bot-bancolombia-toolkit
-git config user.email oficina_open_source@bancolombia.com.co
-git add .
-git commit -m "Upload Changues bot action bancolombia toolkit innersource"
-git push
